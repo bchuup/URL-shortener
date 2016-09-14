5748abfcb453a74acaf30469a1b8c7d1ce98b346
bchuup <bchuup@gmail.com>
2016-09-14 23:25:26 +0000

    created URL submission form; redirected shortened URLs; made it up to URL shortening pt2

--- express_server.js
+++ express_server.js
@@ -3,10 +3,20 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded());
 
 //To specify that we will be using EJS templates, add the following line after defining your app variable
 app.set("view engine", "ejs");
+
+            // function generateRandomString(str) {
+
+            // }
+
+            // function getRandomArbitrary(min, max) {
+            //   return Math.random() * (max - min) + min;
+            // }
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,11 +32,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+//posts
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
