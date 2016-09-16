d8b73465c5e810e1f89aa6a7d6aec60a0fb92279
bchuup <bchuup@gmail.com>
2016-09-16 21:20:12 +0000

    added mongoDB functionality to GET urls/:id

--- express_server.js
+++ express_server.js
@@ -4,18 +4,46 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
 
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded());
 
 //To specify that we will be using EJS templates, add the following line after defining your app variable
 app.set("view engine", "ejs");
 
-let generateRandomString = function(str) {
-  let num = Math.random().toString(36).substr(2, 5)
-  return num
+let generateRandomString = (str) => {
+  let num = Math.random().toString(36).substr(2, 5);
+  return num;
 }
+let collection = null;
+let database = null;
+
+
+// MONGO /////
+MongoClient.connect(MONGODB_URI, (err,db) => {
+  if (err) {
+    // console.log('Could not connect! Unexpected error. Details below.');
+    throw err;
+  }
+  database = db;
+  collection = db.collection("urls");
+});
+
+
+let retrieveLong = function(shortURL, rendercb){
+  collection.find({shortURL: shortURL},{longURL: 1}).toArray((err, results) => {
+    database.close();
+    let shortLong = {
+      shortURL: shortURL,
+      longURL: results[0].longURL
+    };
+    rendercb(shortLong)
+  });
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -36,14 +64,29 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", templateVars);
+  let shortURL = req.params.id;
+  console.log(shortURL);
+  retrieveLong(shortURL, (shortLong) => {
+    res.render("urls_show", shortLong)
+  })
+  // res.end("Hello!");
 });
+
+
+
+
+
+
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+
+
+
+
 
 //delete
 app.delete("/urls/:id", (req, res) => {
