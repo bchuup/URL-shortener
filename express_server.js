8f345b1b1a1983d2c746a219210cd676b7d9d7e3
bchuup <bchuup@gmail.com>
2016-09-18 04:53:06 +0000

    routed last items: get u/:id

--- express_server.js
+++ express_server.js
@@ -1,5 +1,3 @@
-
-
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
@@ -8,24 +6,14 @@ var methodOverride = require('method-override');
 var MongoClient = require("mongodb").MongoClient;
 var MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
 
-
-
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded());
-
 app.set("view engine", "ejs");
 
 var generateRandomString = (str) => {
   var num = Math.random().toString(36).substr(2, 5);
   return num;
 }
-
-var retrieveLong = require("./retrieveLong.js")
-
-// var urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
 
 app.get("/", (req, res) => {
   res.redirect("/urls/new");
@@ -47,7 +35,6 @@ app.get("/urls", (req, res) => {
   })
 });
 
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -62,23 +49,25 @@ var shortURL = req.params.id
         res.render("no_short")
       }
       var resultingObj = results[0]
-        var templateVars = { urls: resultingObj }
+      var templateVars = { urls: resultingObj }
       res.render("urls_show", resultingObj);
     })
   })
 })
 
 app.get("/u/:shortURL", (req, res) => {
+  var shortURL = req.params.shortURL
   MongoClient.connect(MONGODB_URI, (err,db) => {
     var database = db;
     var collection = db.collection("urls");
     collection.find({shortURL:shortURL}).toArray((err, results) =>{
-      var resultingObj = results[0]
-
-          // var templateVars = { urls: resultingObj }
-
-    var longURL = urlDatabase[req.params.shortURL]
-    res.redirect(longURL);
+      var longURL = results[0].longURL
+      if (! results[0]) {
+        res.render("no_short")
+      } else if (longURL.match('http')){
+      res.redirect(longURL);
+      }
+      res.redirect(`http://${longURL}`);
     })
   })
 })
@@ -96,7 +85,7 @@ app.delete("/urls/:id", (req, res) => {
       for (var object of results){
         resultingObj[object.shortURL] = object.longURL
       }
-        var templateVars = { urls: resultingObj }
+      var templateVars = { urls: resultingObj }
       res.render("urls_index", templateVars);
     });
   })
@@ -112,13 +101,11 @@ app.post("/urls", (req, res) => {  // debug statement to see POST parameters // 
     var collection = db.collection("urls");
     collection.insert({shortURL: shortURL, longURL: longURL})
     collection.find().toArray((err, results) =>{
-      console.log(results)
-    var resultingObj = {}
-    for (var object of results){
-      resultingObj[object.shortURL] = object.longURL
-    }
+      var resultingObj = {}
+      for (var object of results){
+        resultingObj[object.shortURL] = object.longURL
+      }
       var redirectPath = `/urls/${shortURL}`
-      // var index = {urls: resultingObj};
       res.redirect(redirectPath);
     })
   })
@@ -133,19 +120,14 @@ app.put("/urls/:id", (req, res) => {
     var collection = db.collection("urls");
     collection.update({shortURL: shortURL}, {shortURL: shortURL, longURL: longURL})
     collection.find().toArray((err, results) =>{
-
       var resultingObj = {}
       for (var object of results){
         resultingObj[object.shortURL] = object.longURL
       }
         var index = {urls: resultingObj};
         res.render("urls_index", index);
-      // res.end('Hello')
     });
   })
-  // urlDatabase[shortURL] = longURL
-  // console.log(longURL, shortURL, urlDatabase[shortURL]);
-  // res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
